53af7baa3fcfe3e41c02d44653ab477324e4080b
PavloTarnovetskyi <98552183+PavloTarnovetskyi@users.noreply.github.com>
2022-09-12 13:47:35 +0300

    add some comments

--- Flaskapp_GitHub/app&dockerfile/app.py
+++ Flaskapp_GitHub/app&dockerfile/app.py
@@ -1,17 +1,21 @@
+# Task: Build a simple application that gets data from open source API and visualize it by simple page.
+
 import requests
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Variables and data for connection with open source API.
 APP_ID = "042ee935522c4ac7851c410321a8cbff"
 ENDPOINT = "https://openexchangerates.org/api/latest.json"
 CURRENCY_DEF_ENDPOINT = "https://openexchangerates.org/api/currencies.json"
 
-response = requests.get(f"{ENDPOINT}?app_id={APP_ID}")
+# assign variables for get data from API
+exchange_rates = requests.get(f"{ENDPOINT}?app_id={APP_ID}").json()["rates"]
 currency_definition = requests.get(f"{CURRENCY_DEF_ENDPOINT}").json()
 
-exchange_rates = response.json()["rates"] 
 
+# define the  URL on application
 @app.route('/', methods=['GET', 'POST'])
 def home():
     currency = {}
@@ -22,12 +26,9 @@ def home():
         currency = { 'title': f'Actual exchange rate USD to {currency_name} ', 
                      'rates': f' 1 USD is {fcur:.3f} {currency_name}'
                }
-        return render_template("home.jinja2", currency_definition=currency_definition, exchange_rates=exchange_rates, currency=currency)
-    return render_template("home.jinja2", exchange_rates=exchange_rates, currency_definition=currency_definition, currency=currency)
+    return render_template("home.jinja2", currency_definition=currency_definition, currency=currency) 
+    
 
-
+# To start the server, and allow the flask receive connection.
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
-
-# use this command in terminal to run that app on your localhosh (you have to install python3):
-# python3 app.py
