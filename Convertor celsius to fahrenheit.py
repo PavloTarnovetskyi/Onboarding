099bf90a099a7f5ca5e184454d87d9d274e1e4ae
PavloTarnovetskyi <98552183+PavloTarnovetskyi@users.noreply.github.com>
2022-09-12 17:29:26 +0300

    delete

--- Convertor celsius to fahrenheit.py
+++
@@ -1,9 +0,0 @@
-celsius = int(input("Enter an integer value of temperature in celsius:"))
-
-
-def fahrenheit(c):
-    return 1.8 * c + 32
-
-
-print("The Fahrenheit equivalent of " + str(celsius) + " degrees Celsius is " + str(float(fahrenheit(celsius))))
-
